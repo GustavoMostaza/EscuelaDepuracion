3007f6d95a627e288dc82f4a88ef5adb734830fa
pepetepes <mostazanew2000@gmail.com>
2024-04-22 23:46:59 -0600

    primer repositorio

---
+++ D.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const pepeButton = document.getElementById('pepe-button');
+    const destinationBody = document.getElementById('destination-body');
+
+    pepeButton.addEventListener('click', function() {
+        fetch('http://localhost/Apis/primera.php')
+            .then(response => response.json())
+            .then(data => {
+                destinationBody.innerHTML = '';
+
+                data.forEach(destination => {
+                    const row = document.createElement('tr');
+                    const destinationData = [destination.Destino, destination.Descripcion, destination.Precio];
+
+                    destinationData.forEach(cellData => {
+                        const cell = document.createElement('td');
+                        cell.textContent = cellData;
+                        cell.classList.add('border', 'px-4', 'py-2');
+                        row.appendChild(cell);
+                    });
+
+                    destinationBody.appendChild(row);
+                });
+
+                destinationTable.classList.remove('hidden');
+            })
+            .catch(error => console.error('Error al obtener los datos:', error));
+    });
+});
